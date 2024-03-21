63c8d1407f94d6cc7991115e14328d8d4e38b604
Samir Khare <k.samir@wustl.edu>
2024-03-21 09:54:34 -0500

    Started javascript implementation

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+let img = [
+    "Images/benjamin-lehman-xnTcEJm5IZw-unsplash.jpg",
+    "Images/clement-proust-x8pdFo5tgVE-unsplash.jpg",
+    "Images/denis-tuksar-pugdAUdJOyQ-unsplash.jpg",
+    "Images/krisna-yuda-otS0X-RqoQs-unsplash.jpg",
+    "Images/makomakt-J5LZCaYhhtM-unsplash.jpg",
+    "Images/mos-knKTQrptRB4-unsplash.jpg",
+    "Images/noam-cohen-DhTIf0QdP1w-unsplash.jpg",
+    "Images/peter-thomas--P5CzqQ77hY-unsplash.jpg",
+    "Images/peter-thomas-Ye_5G755Ls0-unsplash.jpg",
+    "Images/tom-podmore-tnAtAIG_9Mo-unsplash.jpg",
+    "Images/wenya-luo-4cPpG35HORU-unsplash.jpg",
+    "Images/yves-cedric-schulze-84vI7X58HMs-unsplash.jpg"
+]
+document.getElementsByClassName("picture").addEventListener("click", enlarge);
+function enlarge() {
+    document.getElementsByClassName("MyElement").className = "MyClass"
+}
+document.getElementsByClassName("close").addEventListener("click", closeButton);
+function closeButton() {
 
+}
+document.getElementsByClassName("next").addEventListener("click", nextButton);
+function nextButton() {
+
+}
+document.getElementsByClassName("previous").addEventListener("click", previousButton);
+function previousButton() {
+
+}
